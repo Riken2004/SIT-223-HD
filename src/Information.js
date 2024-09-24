ee01fcfb69b042a982c71761f7d3b5e5bb2b2fbf
Riken2004 <139823114+Riken2004@users.noreply.github.com>
2024-09-25 04:08:30 +1000

    Update Information.js

--- src/Information.js
+++ src/Information.js
@@ -5,17 +5,19 @@ const Info = () => {
     return (
         <div className="Part">
             <h2> What do you want to ask or share </h2>
-            <p>This section is designed based on the type of the post. It could be developed by conditional rendering.
-                <a className='color'> For post an article, the following section would be appeared.</a>
+            <p>
+                This section is designed based on the type of the post. It could be developed by conditional rendering.
+                {/* Changed <a> to a button or added href */}
+                <a href="/post-article" className='color'> For post an article, the following section would appear.</a>
             </p>
             <label className='title'>Title</label>
             <input type="text" className="Box" placeholder="Enter a descriptive title"/>
             <h3 className='abstractclass'> Abstract </h3>
-            <textarea placeholder='Enter a 1- paragraph abstract'/>
+            <textarea placeholder='Enter a 1-paragraph abstract'/>
             <h3 className='abstractclass'>Article text</h3>
-            <textarea placeholder='Enter a 1- paragraph abstract'/>
+            <textarea placeholder='Enter the full text of your article'/>
             <label>Tags</label>
-            <input className= 'Box2' type="text" placeholder="please add up to 3 tags to describe what your article is about e.g., java"/>
+            <input className='Box2' type="text" placeholder="Please add up to 3 tags to describe what your article is about e.g., Java"/>
             <button className="btn">Post</button>
         </div>
     );
