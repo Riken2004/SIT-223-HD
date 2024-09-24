5ecb8ed4c05f16060272832d2d2acd8eb01fb188
Riken2004 <139823114+Riken2004@users.noreply.github.com>
2024-09-25 04:13:14 +1000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';  
 import App from './App';
 
-test('renders New Post text', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const newPostElement = screen.getByText(/New Post/i);
-  expect(newPostElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
